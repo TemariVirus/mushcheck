621896172826006a4d691c457f31f42fc5d6b01d
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-01 01:24:11 +0800

    mushroom error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	<h1>{$page.status}<br />{$page.error?.message}</h1>
+</div>
+
+<style>
+	div {
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+		height: 100vh;
+	}
+
+	h1 {
+		font-size: 5rem;
+	}
+</style>
--- app/src/routes/mushrooms/+page.svelte
+++ app/src/routes/mushrooms/+page.svelte
@@ -2,36 +2,54 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 
-	const mushroom_url = 'https://5yrf3rrviwwrb6xhu77445ssiy0kcsqb.lambda-url.us-east-1.on.aws/';
-
 	let name: string;
-	let edibility: boolean;
+	let edible: boolean;
 	let description: string;
-	let furtherReading: string;
-	let imageUrl: string;
+	let extras_url: string;
+	let image_url: string;
+	let error = false;
 
-	// Get mushroom info from db
+	// Get mushroom info from DB
 	onMount(async () => {
-		let name = $page.url.searchParams.get('name');
-		let result = await fetch(`${mushroom_url}?name=${name}`).then((res) => res.json());
+		name = $page.url.searchParams.get('name') ?? '';
+		let result = await fetch(
+			`https://5yrf3rrviwwrb6xhu77445ssiy0kcsqb.lambda-url.us-east-1.on.aws?name=${name}`
+		)
+			.then((r) => (r.ok ? r.json() : Promise.reject(r)))
+			.catch(async (e) => {
+				error = true;
+				const err_msg = await e.text();
+				throw new Error(err_msg);
+			});
+
 		console.log(result);
-		({ name, edibility, description, furtherReading, imageUrl } = result);
+		({ description, extras_url, image_url } = result);
+		edible = result.edible == 0;
 	});
 </script>
 
 <section>
-	<img src={imageUrl} alt={`A ${name}`} />
+	{#if error}
+		<h1>Error 404</h1>
+		<p>Mushroom not found</p>
+		<p><a href="/">Go home</a></p>
+	{:else}
+		<img src={image_url} alt={`A ${name}`} />
 
-	<h2>{name}</h2>
-	<p>Edibility: {edibility}</p>
-	<p>{description}</p>
+		<h2>{name}</h2>
+		<p>Edible: {edible ? 'Yes' : 'No'}</p>
+		<p>{description}</p>
 
-	{#if furtherReading}
-		<p>Further reading: <a href={furtherReading}>{furtherReading}</a></p>
+		{#if extras_url}
+			<p>Further reading: <a href={extras_url}>{extras_url}</a></p>
+		{/if}
 	{/if}
 </section>
 
 <style>
+	section {
+		text-align: center;
+	}
 	h2 {
 		text-align: center;
 	}
