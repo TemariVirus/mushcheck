93a7d9a31d8708e37dbb90209714473838e2c287
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-02 17:31:48 +0800

    Edit mushrooms page

--- app/src/routes/mushrooms/+page.svelte
+++ app/src/routes/mushrooms/+page.svelte
@@ -2,11 +2,11 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	const { name, image_url, edible, description } = data.props as any;
+	const { name, image_url, edible, description } = data.props;
 </script>
 
 <section>
-	<h2>{name}</h2>
+	<h1>{name}</h1>
 	<img src={image_url} alt={`A ${name}`} />
 	<p>Edible: {edible ? 'Yes' : 'No'}</p>
 	{@html description}
@@ -20,7 +20,7 @@
 		width: 50%;
 	}
 
-	h2 {
+	h1 {
 		text-align: center;
 	}
 
