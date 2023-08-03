098a693b0e1bd14a54b1e4430c284c86f287fdd1
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-03 09:34:07 +0800

    Revert "Edit mushrooms page"
    This reverts commit 93a7d9a31d8708e37dbb90209714473838e2c287.

--- app/src/routes/mushrooms/+page.svelte
+++ app/src/routes/mushrooms/+page.svelte
@@ -2,11 +2,11 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	const { name, image_url, edible, description } = data.props;
+	const { name, image_url, edible, description } = data.props as any;
 </script>
 
 <section>
-	<h1>{name}</h1>
+	<h2>{name}</h2>
 	<img src={image_url} alt={`A ${name}`} />
 	<p>Edible: {edible ? 'Yes' : 'No'}</p>
 	{@html description}
@@ -20,7 +20,7 @@
 		width: 50%;
 	}
 
-	h1 {
+	h2 {
 		text-align: center;
 	}
 
