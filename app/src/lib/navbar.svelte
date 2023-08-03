11a037fb5983e7dfb9de76c8523bce905e7fc9b0
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-03 16:17:00 +0800

    Finish /mushrooms page

---
+++ app/src/lib/navbar.svelte
@@ -0,0 +1,28 @@
+<nav>
+	<a href="/" data-sveltekit-reload>Home</a>
+	<a href="/mushrooms" data-sveltekit-reload>Dictionary</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		background-color: #333;
+		color: #fff;
+		padding: 1rem;
+		margin: 0;
+		margin-bottom: 1rem;
+		top: 0;
+	}
+
+	a {
+		text-decoration: none;
+		color: #fff;
+		font-size: 28px;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
