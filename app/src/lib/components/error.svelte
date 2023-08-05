1b39f60e62e8733396b2d05d703682dd4bbf5ae3
Zemogus <kiyonnxperiaz@gmail.com>
2023-08-05 21:27:49 +0800

    scan html skeleton

---
+++ app/src/lib/components/error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let status: number;
+	export let message: string;
+</script>
+
+<div>
+	<h1>{status}<br />{message}</h1>
+</div>
+
+<style>
+	div {
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+		height: 100vh;
+	}
+
+	h1 {
+		font-size: 5rem;
+		max-width: 1000px;
+	}
+</style>
